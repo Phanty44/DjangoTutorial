8741fc8afaee2391ba106aa9a46e0a50c64d97b5
Phanty <saitek123@op.pl>
2023-05-31 19:34:55 +0200

    Initial commit with creating producks

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+    summary = models.TextField(default="Test")
