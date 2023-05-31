7e2ff9013ca27967d11d7cbaa88d6d54cd8d00f1
Phanty <saitek123@op.pl>
2023-05-31 20:04:13 +0200

    Altering model's fields

--- products/models.py
+++ products/models.py
@@ -5,7 +5,8 @@ from django.db import models
 # https://docs.djangoproject.com/en/2.1/ref/models/fields/
 
 class Product(models.Model):
-    name = models.CharField(max_length=120) # max length of 120
-    description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10000, decimal_places=2) # 9999,99
+    name = models.CharField(max_length=120)  # max length of 120
+    description = models.TextField(blank=True, null=True)  # blank = field not required to create
+    price = models.DecimalField(max_digits=10000, decimal_places=2)  # 9999,99
     summary = models.TextField(default="Test")
+    discount = models.BooleanField()  # new field must be added to all existing products
